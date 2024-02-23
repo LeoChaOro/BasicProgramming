e0734e7422a963b4c42f59c5827fbcbce663d1e6
LeoChaOro <160760304+LeoChaOro@users.noreply.github.com>
2024-02-23 10:03:01 -0500

    Tarea6

---
+++ DataTypes/mainStringTypes2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	//Include <string>
+	#include <string>
+	
+	//Create a string variable
+	string greeting = "Hello";
+	
+	//output string value
+	cout <<greeting;
+	
+	return 0;
+}
