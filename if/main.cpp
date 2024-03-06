fe779c0fb651ba721ed61201cf40a0936b0421a7
LeoChaOro <160760304+LeoChaOro@users.noreply.github.com>
2024-03-06 00:30:12 -0500

    tarea1

---
+++ if/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int i=0;
+	int e;
+	int o;
+	cout<<i<<endl;
+	
+	if(i==0) {
+		i=i+1;
+		}
+		cout<<i<<endl;
+		if(i==1){
+			i=i+1;
+		}
+		cout<<i<<endl;
+		if(i==2){
+			i=i+1;
+		}
+		cout<<i<<endl;
+		if(i==3){
+			i=i+1;
+		}
+		cout<<i<<endl;
+		if(i==4){
+			i=i+1;
+		}
+		cout<<i<<endl;
+		if(i==5){
+			i=i+1;
+		}
+		cout<<i<<endl;
+		if(i==6){
+			i=i+1;
+		}
+		cout<<i<<endl;
+		if(i==7){
+			i=i+1;
+		}
+		cout<<i<<endl;
+		if(i==8){
+			i=i+1;
+		}
+		cout<<i<<endl;
+		if (i==9){
+		cout<<"numero es igual a 9"<<endl;	
+	}
+		
+		
+		
+	return 0;
+}
