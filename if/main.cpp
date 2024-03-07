d4ec65e810744a29c5e546c1d83592f7411c0489
LeoChaOro <160760304+LeoChaOro@users.noreply.github.com>
2024-03-07 05:58:53 -0600

    ejer1/07-03-24

--- if/main.cpp
+++ if/main.cpp
@@ -5,50 +5,47 @@ using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	int i=0;
+int i=0;
 	cout<<i<<endl;
 	
-	if(i==0) {
+	if(i<9) {
 		i=i+1;
 		}
 		cout<<i<<endl;
-		if(i==1){
-			i=i+1;
-		}
-		cout<<i<<endl;
-		if(i==2){
-			i=i+1;
-		}
-		cout<<i<<endl;
-		if(i==3){
-			i=i+1;
-		}
-		cout<<i<<endl;
-		if(i==4){
-			i=i+1;
-		}
-		cout<<i<<endl;
-		if(i==5){
-			i=i+1;
-		}
-		cout<<i<<endl;
-		if(i==6){
-			i=i+1;
-		}
-		cout<<i<<endl;
-		if(i==7){
-			i=i+1;
-		}
-		cout<<i<<endl;
-		if(i==8){
-			i=i+1;
-		}
-		cout<<i<<endl;
-		if (i==9){
-		cout<<"numero es igual a 9"<<endl;	
+	if(i<9){
+		i=i+1;
 	}
-		
-		
+cout<<i<<endl;
+    if(i<9){
+		i=i+1;
+	}
+cout<<i<<endl;
+    if(i<9){
+		i=i+1;
+	}
+cout<<i<<endl;
+if(i<9){
+		i=i+1;
+	}
+cout<<i<<endl;
+if(i<9){
+		i=i+1;
+	}
+cout<<i<<endl;
+if(i<9){
+		i=i+1;
+	}
+cout<<i<<endl;
+if(i<9){
+		i=i+1;
+	}
+cout<<i<<endl;
+if(i<9){
+		i=i+1;
+	}
+cout<<i<<endl;
+
+	
 		
 	return 0;
 }
