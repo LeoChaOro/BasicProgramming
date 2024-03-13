802de9dd73fed4daec40222260c6a35d54bea52b
LeoChaOro <160760304+LeoChaOro@users.noreply.github.com>
2024-03-13 11:06:29 -0500

    Add files via upload

---
+++ whileloop/ejerciciotutarialspoint.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+		//local variable declaration:
+		int a=10;
+		//while loop execution
+		while(a<20){
+			cout<<"value of a: "<<a<<endl;
+			a++;
+			
+		}
+	
+	return 0;
+}
