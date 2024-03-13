4e62afcd487cf51a93cb69387511ea99d48efc42
LeoChaOro <160760304+LeoChaOro@users.noreply.github.com>
2024-03-13 11:25:16 -0500

    Add files via upload

---
+++ whileloop/ejer1begginersbook.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	 int i=1;
+   /* The loop would continue to print
+    * the value of i until the given condition
+    * i<=6 returns false.
+    */
+   while(i<=6){
+      cout<<"Value of variable i is: "<<i<<endl; i++;
+   }
+
+	return 0;
+}
